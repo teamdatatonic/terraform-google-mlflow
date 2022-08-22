804f88bdf11dbde8ec4b59605aea21a19e813e4f
kev-in047 <kevin.karobia@datatonic.com>
2022-08-22 11:48:45 +0100

    Feature: added more variables to module inputs

--- examples/complete_config/main.tf
+++ examples/complete_config/main.tf
@@ -1,5 +1,5 @@
 module "mlflow_service" {
-  source  = "teamdatatonic/terraform-google-mlflow"
+  source  = "teamdatatonic/mlflow/google"
   version = "1.0.0"
 
   project_id          = var.project_id
@@ -7,10 +7,12 @@ module "mlflow_service" {
   zone                = var.zone
   mlflow_docker_image = var.mlflow_docker_image
   network_name        = var.network_name
+  create_brand        = var.create_brand
   brand_name          = var.brand_name
   support_email       = var.support_email
   oauth_client_id     = var.oauth_client_id
   oauth_client_secret = var.oauth_client_secret
   domain              = var.domain
   webapp_users        = var.webapp_users
+  storage_uniform     = var.storage_uniform
 }
